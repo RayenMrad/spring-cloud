2fb9f34799bd1f2456ae0a77c82b27647966c8f7
Rayen Mrad <rayenmrad80@gmail.com>
2025-11-13 21:18:23 +0100

    section 8

---
+++ section8/rayen-spring-cloud/chanson-microservice/src/main/java/com/rayen/chansons/service/APIClient.java
@@ -0,0 +1,15 @@
+package com.rayen.chansons.service;
+
+import com.rayen.chansons.dto.GenreDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(url = "http://localhost:8081", value = "GENRE")
+@FeignClient(name = "GENRE")
+public interface APIClient {
+    @GetMapping("api/genres/{genre-code}")
+    GenreDto getGenByCode(@PathVariable("genre-code")
+                               String genreCode
+    );
+}
---
+++ section8/rayen-spring-cloud/genre-microservice/src/test/java/com/rayen/genre/GenreMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayen.genre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenreMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
